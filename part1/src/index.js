c8c08b4ac3c420c2a87143ec359d623a62d967ca
Matt <mvroom@redventures.com>
2019-11-01 15:00:42 -0400

    guide finished, exercises next

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,21 +1,41 @@
 import ReactDOM from "react-dom";
 import React, { useState } from "react";
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const History = props => {
+    if (props.allClicks.length === 0) {
+        return <div>the app is used by pressing the buttons</div>;
+    }
+
+    return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = props => {
-    const [counter, setCounter] = useState(0);
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+    const [allClicks, setAll] = useState([]);
 
-    const setToValue = value => () => setCounter(value);
+    // state HAS to be changed by a new object, not adding directly
+    const handleLeftClick = () => {
+        setAll(allClicks.concat("L"));
+        setLeft(left + 1);
+    };
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat("R"));
+        setRight(right + 1);
+    };
 
     return (
         <div>
-            <Display counter={counter} />
-            <Button onClick={setToValue(counter + 1)} text="plus" />
-            <Button onClick={setToValue(counter - 1)} text="minus" />
-            <Button onClick={setToValue(0)} text="zero" />
+            <div>
+                {left}
+                <Button onClick={handleLeftClick} text="left" />
+                <Button onClick={handleRightClick} text="right" />
+                {right}
+                <History allClicks={allClicks} />
+            </div>
         </div>
     );
 };
